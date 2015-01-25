b81c77832830ee21ac1bf55128d8925e2aa18e77
leon <venus.oso@gmail.com>
2015-01-26 01:40:34 +0800

    提取常量类

---
+++ src/main/com/huijia/eap/quiz/data/QuizConstant.java
@@ -0,0 +1,12 @@
+package com.huijia.eap.quiz.data;
+
+public class QuizConstant {
+
+	/**
+	 * 试卷类型
+	 */
+	public final static short QUIZ_TYPE_STANDALONE = 0;		// 独立试卷
+	public final static short QUIZ_TYPE_PARENT = 1;			// 父试卷
+	public final static short QUIZ_TYPE_CHILD = 2;			// 子试卷
+	
+}
